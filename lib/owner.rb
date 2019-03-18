23f538c144c351513fbf7ae9d42497fabb24b572
Serena Berry <siberry226@gmail.com>
2019-03-18 20:57:20 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -47,5 +47,9 @@ class Owner
   def play_with_cats
     pets[:cats].map {|cat| cat.mood = "happy" }
   end
+  
+  def feed_fish
+    pets[:fishes].map {|fish| fish.mood = "happy" }
+  end
     
 end
